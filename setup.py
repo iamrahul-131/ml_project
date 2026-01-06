6be4246961c3cd757b29f40d63c774f68170afbd
John Doe <rahul22035662@gmail.com>
2026-01-06 10:26:24 +0530

    logging and exception

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages,setup
+from typing import List
+HYPHEN_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    Docstring for get_requirements
+    
+    :param file_path: Description
+    :type file_path: str
+    :return: Description
+    :rtype: List[str]
+    This function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n"," ") for req in requirements]
+
+        if HYPHEN_DOT in requirements:
+            requirements.remove(HYPHEN_DOT)
+setup(
+    name="ml_project",
+    version="0.0.1",
+    author="Rahul Giri",
+    author_email="rahul22035662@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt")
+)
---
+++ src/exception.py
@@ -0,0 +1,19 @@
+import sys
+
+def error_message_details(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    exception_file_name=exc_tb.tb_frame.f_code.co_filename##tells in which file exception occured basically tells the file name
+    exception_line=exc_tb.tb_lineno##at which line number exception occured
+    exception_error=str(error)#what is error message when exception occured 
+    error_message="Error occurd in python script name [{0}] line number [{1}] error message[{2}]".format()
+    exception_file_name,exception_line,exception_error
+
+    return error_message
+
+class CustonException(Exception):
+    def _init__(self,error_message,error_detail:sys): #Constructor
+        super().__init__(error_message)
+        self.error_message=error_message_details(error_message,error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
