2fbe46030a67a03a9fe2d0d14689ca43545d36ac
John Doe <rahul22035662@gmail.com>
2026-01-07 22:13:03 +0530

    Model Training

--- src/utils.py
+++ src/utils.py
@@ -19,3 +19,33 @@ def save_object(file_path,obj):## this function is used to save preprocessor obj
 
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+from sklearn.metrics import r2_score
+import sys
+
+def evaluate_models(x_train, y_train, x_test, y_test, models):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+            # Train model
+            model.fit(x_train, y_train)
+
+            # Predictions
+            y_train_pred = model.predict(x_train)
+            y_test_pred = model.predict(x_test)
+
+            # Scores
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            # Store test score
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise Exception(e)
