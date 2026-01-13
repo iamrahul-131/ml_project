e351f315b808e8a96f04a65de721328df23b47b7
John Doe <rahul22035662@gmail.com>
2026-01-13 10:14:37 +0530

    Fix Render Deploy

--- app.py
+++ app.py
@@ -36,6 +36,10 @@ def predict_datapoint():
         predict_pipeline=PredictPipeline()
         results=predict_pipeline.predict(pred_df)
         return render_template('home.html',results=results[0])
-    
-if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True)
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
